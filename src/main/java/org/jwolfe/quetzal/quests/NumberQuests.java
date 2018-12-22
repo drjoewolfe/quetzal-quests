e12263f191e59189289af70d0bdf94249d8b8966
John Wolfe <dr.joewolfe@gmail.com>
2018-12-22 19:50:00 +0530

    +1 to number-quests

--- src/main/java/org/jwolfe/quetzal/quests/NumberQuests.java
+++ src/main/java/org/jwolfe/quetzal/quests/NumberQuests.java
@@ -2,29 +2,54 @@ package org.jwolfe.quetzal.quests;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class NumberQuests {
-    public static List<Integer> getPrimeNumbersSmallerThanEqualToMaxUsingSieveOfEratosthenes(int max) {
-        List<Integer> primeNumbers = new ArrayList<>();
+	public static List<Integer> getPrimeNumbersSmallerThanEqualToMaxUsingSieveOfEratosthenes(int max) {
+		List<Integer> primeNumbers = new ArrayList<>();
 
-        boolean[] isPrime = new boolean[max + 1];
-        Arrays.fill(isPrime, true);
+		boolean[] isPrime = new boolean[max + 1];
+		Arrays.fill(isPrime, true);
 
-        for(int p = 2; p * p <= max; p++) {
-            if(isPrime[p]) {
-                for (int i = p*p; i <= max; i+=p) {
-                    isPrime[i] = false;
-                }
-            }
-        }
+		for (int p = 2; p * p <= max; p++) {
+			if (isPrime[p]) {
+				for (int i = p * p; i <= max; i += p) {
+					isPrime[i] = false;
+				}
+			}
+		}
 
-        for (int i = 2; i <= max; i++) {
-            if(isPrime[i]) {
-                primeNumbers.add(i);
-            }
-        }
+		for (int i = 2; i <= max; i++) {
+			if (isPrime[i]) {
+				primeNumbers.add(i);
+			}
+		}
 
-        return primeNumbers;
-    }
+		return primeNumbers;
+	}
+
+	public static int minStepsRequiredToFindNumberInNumberLineForNStepsPerIteration(int number) {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(0);
+
+		int iteration = 1;
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			for (int i = 0; i < size; i++) {
+				Integer n = queue.poll();
+				if (n == number) {
+					return iteration - 1;
+				}
+
+				queue.offer(n + iteration);
+				queue.offer(n - iteration);
+			}
+
+			iteration++;
+		}
+
+		return -1;
+	}
 }
--- src/test/java/org/jwolfe/quetzal/quests/NumberQuestsTest.java
+++ src/test/java/org/jwolfe/quetzal/quests/NumberQuestsTest.java
@@ -9,21 +9,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class NumberQuestsTest {
 
-    @Test
-    void getPrimeNumbersSmallerThanEqualToMaxUsingSieveOfEratosthenes() {
-        List<Integer> primeNumbers;
-        int[] expectedPrimeNumbers;
+	@Test
+	void getPrimeNumbersSmallerThanEqualToMaxUsingSieveOfEratosthenes() {
+		List<Integer> primeNumbers;
+		int[] expectedPrimeNumbers;
 
-        primeNumbers = NumberQuests.getPrimeNumbersSmallerThanEqualToMaxUsingSieveOfEratosthenes(10);
-        expectedPrimeNumbers = Utilities.constructArray(2, 3, 5,7);
-        assertArrayEquals(expectedPrimeNumbers, primeNumbers.stream().mapToInt(i->i).toArray());
+		primeNumbers = NumberQuests.getPrimeNumbersSmallerThanEqualToMaxUsingSieveOfEratosthenes(10);
+		expectedPrimeNumbers = Utilities.constructArray(2, 3, 5, 7);
+		assertArrayEquals(expectedPrimeNumbers, primeNumbers.stream().mapToInt(i -> i).toArray());
 
-        primeNumbers = NumberQuests.getPrimeNumbersSmallerThanEqualToMaxUsingSieveOfEratosthenes(20);
-        expectedPrimeNumbers = Utilities.constructArray(2, 3, 5, 7, 11, 13, 17, 19);
-        assertArrayEquals(expectedPrimeNumbers, primeNumbers.stream().mapToInt(i->i).toArray());
+		primeNumbers = NumberQuests.getPrimeNumbersSmallerThanEqualToMaxUsingSieveOfEratosthenes(20);
+		expectedPrimeNumbers = Utilities.constructArray(2, 3, 5, 7, 11, 13, 17, 19);
+		assertArrayEquals(expectedPrimeNumbers, primeNumbers.stream().mapToInt(i -> i).toArray());
 
-        primeNumbers = NumberQuests.getPrimeNumbersSmallerThanEqualToMaxUsingSieveOfEratosthenes(30);
-        expectedPrimeNumbers = Utilities.constructArray(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
-        assertArrayEquals(expectedPrimeNumbers, primeNumbers.stream().mapToInt(i->i).toArray());
-    }
+		primeNumbers = NumberQuests.getPrimeNumbersSmallerThanEqualToMaxUsingSieveOfEratosthenes(30);
+		expectedPrimeNumbers = Utilities.constructArray(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
+		assertArrayEquals(expectedPrimeNumbers, primeNumbers.stream().mapToInt(i -> i).toArray());
+	}
+
+	@Test
+	void minStepsRequiredToFindNumberInNumberLineForNStepsPerIteration() {
+		assertEquals(4, NumberQuests.minStepsRequiredToFindNumberInNumberLineForNStepsPerIteration(10));
+		assertEquals(5, NumberQuests.minStepsRequiredToFindNumberInNumberLineForNStepsPerIteration(13));
+		assertEquals(7, NumberQuests.minStepsRequiredToFindNumberInNumberLineForNStepsPerIteration(20));
+	}
 }
